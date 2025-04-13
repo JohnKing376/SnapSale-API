c4d62417147af80822fbd664c1903b4ee7002e9b
JohnKing376 <johnolufemikingiyanu@gmail.com>
2025-04-13 22:45:54 +0100

    environment configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import databaseConfig from './config/database/database.config';
+import appConfig from './config/app.config';
+import environmentValidator from './config/validator/environment.validator';
+
+const ENV = process.env.NODE_ENV;
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig, appConfig],
+      envFilePath: !ENV ? `.env` : `.env.${ENV}`,
+      validationSchema: environmentValidator,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        database: configService.get('database.name'),
+        port: configService.get('database.port'),
+        host: configService.get('database.host'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        autoLoadEntities: configService.get('database.autoLoadEntities'),
+        synchronize: configService.get('database.synchronize'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
