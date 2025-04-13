90b0e6466f92c15bd1ef3ddd010b451dea9c0963
JohnKing376 <johnolufemikingiyanu@gmail.com>
2025-04-13 22:31:31 +0100

    Revert "feat: environment configs"
    This reverts commit 2d420ef7abb3e388c85e385f5036356cdc3baf0e.

--- src/config/app.config.ts
+++
@@ -1,6 +0,0 @@
-import { registerAs } from '@nestjs/config';
-import * as process from 'node:process';
-
-export default registerAs('app', () => ({
-  NODE_ENV: process.env.NODE_ENV || 'production',
-}));
