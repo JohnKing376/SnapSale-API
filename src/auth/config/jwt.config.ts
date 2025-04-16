e8c670b45cd63dbb016e0f3d0625992c91a70057
JohnKing376 <johnolufemikingiyanu@gmail.com>
2025-04-16 14:51:59 +0100

    feat: add new env variables & validation

--- src/auth/config/jwt.config.ts
+++ src/auth/config/jwt.config.ts
@@ -5,4 +5,5 @@ export default registerAs('jwtConfig', () => ({
   audience: process.env.JWT_TOKEN_AUDIENCE,
   issuer: process.env.JWT_TOKEN_ISSUER,
   accessTokenTTl: Number(process.env.JWT_ACCESS_TOKEN_TTL),
+  refreshTokenTTL: Number(process.env.JWT_REFRESH_TOKEN_TTL),
 }));
