974980492c1f86be3ed9e3da512b5b7f586c9a03
JohnKing376 <johnolufemikingiyanu@gmail.com>
2025-04-18 22:13:20 +0100

    feat: edit abstract model

--- src/common/models/abstract-model.entity.ts
+++ src/common/models/abstract-model.entity.ts
@@ -1,26 +1,39 @@
-import { Entity, Column, BeforeInsert } from 'typeorm';
+import { Column, BeforeInsert, PrimaryGeneratedColumn } from 'typeorm';
 import { DateTime } from 'luxon';
 import { createId } from '@paralleldrive/cuid2';
 
-@Entity()
-export class AbstractModel {
+const dateTransformer = {
+  to(value: Date | null): Date | null {
+    return value;
+  },
+  from(value: Date | null): string | null {
+    return value ? DateTime.fromJSDate(value).setZone('utc').toISO() : null;
+  },
+};
+
+export abstract class AbstractModel {
+  @PrimaryGeneratedColumn('increment')
+  declare id: number;
+
   @Column({
     type: 'varchar',
     unique: true,
   })
-  identifier: string;
+  declare identifier: string;
 
   @Column({
     type: 'timestamp',
     nullable: true,
+    transformer: dateTransformer,
   })
-  createdAt: DateTime;
+  declare createdAt: Date;
 
   @Column({
     type: 'timestamp',
     nullable: true,
+    transformer: dateTransformer,
   })
-  updatedAt: DateTime;
+  declare updatedAt: Date;
 
   @BeforeInsert()
   generateIdentifier() {
