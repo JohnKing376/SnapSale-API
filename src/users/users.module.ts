18adab3840cc8e43c9cb54b9c8cb22e32ecb228e
JohnKing376 <johnolufemikingiyanu@gmail.com>
2025-05-03 21:50:45 +0100

    feat: add OtpToken Module as a circular dependency

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,9 +6,14 @@ import { CreateUserProvider } from './providers/create-user.provider';
 import User from './entities/user.entity';
 import { AuthModule } from '../auth/auth.module';
 import { FindUserByEmailProvider } from './providers/find-user-by-email.provider';
+import { OtpTokenModule } from '../otp-token/otp-token.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    forwardRef(() => AuthModule),
+    forwardRef(() => OtpTokenModule),
+  ],
   providers: [UsersService, CreateUserProvider, FindUserByEmailProvider],
   controllers: [UsersController],
   exports: [UsersService],
