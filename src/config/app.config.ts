542315f5456d0e33d857385efb72bdf3306326e2
JohnKing376 <johnolufemikingiyanu@gmail.com>
2025-05-07 18:43:24 +0100

    feat: add business_mail && corrections

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,8 +2,9 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
   NODE_ENV: process.env.NODE_ENV || 'production',
-  MAIL_HOST: process.env.MAIL_HOST,
-  MAIL_PORT: Number(process.env.MAIL_PORT),
-  MAIL_USER: process.env.MAIL_USER,
-  MAIL_PASSWORD: process.env.MAIL_PASSWORD,
+  mail_host: process.env.MAIL_HOST,
+  mail_port: Number(process.env.MAIL_PORT),
+  mail_user: process.env.MAIL_USERNAME,
+  mail_password: process.env.MAIL_PASSWORD,
+  business_mail: process.env.BUSINESS_MAIL
 }));
