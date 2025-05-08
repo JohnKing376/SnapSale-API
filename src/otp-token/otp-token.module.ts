f574deb885e05729afbc0dc92b570bb5610b7544
JohnKing376 <johnolufemikingiyanu@gmail.com>
2025-05-08 10:02:22 +0100

    chore: update otp-token module

--- src/otp-token/otp-token.module.ts
+++ src/otp-token/otp-token.module.ts
@@ -4,13 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import OtpToken from './entities/otp-token.entity';
 import { UsersModule } from '../users/users.module';
 import { OtpTokenController } from './controllers/otp-token.controller';
+import { VerifyTokenProvider } from './providers/verify-token.provider';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([OtpToken]),
     forwardRef(() => UsersModule),
   ],
-  providers: [OtpTokenService],
+  providers: [OtpTokenService, VerifyTokenProvider],
   exports: [OtpTokenService],
   controllers: [OtpTokenController],
 })
