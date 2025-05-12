6334f57a85e30a39c6aaba1d17241125b1d332b7
JohnKing376 <johnolufemikingiyanu@gmail.com>
2025-05-12 21:58:00 +0100

    feat: add update-user to user-service

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,12 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserProvider } from './create-user.provider';
 import { CreateUserOptions } from '../interfaces/create-user.interface';
 import User from '../entities/user.entity';
+import { UpdateUserOptions } from '../types/update-user.types';
 
 @Injectable()
 export class UsersService {
+  private readonly logger = new Logger('UsersService');
   constructor(
     /**
      * Import User Repository
@@ -26,9 +33,10 @@ export class UsersService {
    * @param createUserOptions
    * @memberOf UsersService
    */
-  public async createUser(
-    createUserOptions: CreateUserOptions,
-  ): Promise<{ newUser: User; accessToken: string }> {
+  public async createUser(createUserOptions: CreateUserOptions): Promise<{
+    newUser: User;
+    tokens: { accessTokens: string; refreshTokens: string };
+  }> {
     return await this.createUserProvider.createUser(createUserOptions);
   }
 
@@ -64,5 +72,36 @@ export class UsersService {
    */
   public async findUserByIdentifier(identifier: string): Promise<User | null> {
     return await this.userRepository.findOneBy({ identifier });
+  }
+
+  /**
+   * @public
+   * @description Method to update a user
+   * @param identifier
+   * @param updateUserOptions
+   * @memberOf UsersService
+   */
+  public async updateUser(
+    identifier: string,
+    updateUserOptions: UpdateUserOptions,
+  ) {
+    const user = await this.findUserByIdentifier(identifier);
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    try {
+      const updatedUser = this.userRepository.merge(user, updateUserOptions);
+      updatedUser.updatedAt = new Date();
+      await this.userRepository.save(updatedUser);
+      return await this.findOneById(updatedUser.id);
+    } catch (error) {
+      this.logger.error(
+        `[UPDATE-USER-SERVICE]: ${JSON.stringify(error, null, 2)}`,
+      );
+      throw new InternalServerErrorException(
+        'Something went wrong while trying to update the user',
+      );
+    }
   }
 }
---
+++ src/users/types/update-user.types.ts
@@ -0,0 +1,3 @@
+import { CreateUserOptions } from '../interfaces/create-user.interface';
+
+export type UpdateUserOptions = Partial<CreateUserOptions>;
