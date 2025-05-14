cc39e3539c09af49a8bc7fee0772e1b767bc260c
JohnKing376 <johnolufemikingiyanu@gmail.com>
2025-05-14 23:20:08 +0100

    feat: create password.module

--- src/auth/password-management/password.module.ts
+++ src/auth/password-management/password.module.ts
@@ -5,15 +5,27 @@ import { AuthModule } from '../auth.module';
 import { UsersModule } from '../../users/users.module';
 import { BcryptProvider } from '../providers/bcrypt.provider';
 import { HashingProvider } from '../providers/hashing.provider';
+import { BullModule } from '@nestjs/bullmq';
+import { PASSWORD_RESET_MAIL } from './constants/password-management-constants';
+import { PasswordResetMailProcessor } from './jobs/password-reset-mail.processor';
+import { OtpTokenModule } from '../../otp-token/otp-token.module';
 
 @Module({
-  imports: [forwardRef(() => AuthModule), forwardRef(() => UsersModule)],
+  imports: [
+    forwardRef(() => AuthModule),
+    forwardRef(() => UsersModule),
+    OtpTokenModule,
+    BullModule.registerQueue({
+      name: PASSWORD_RESET_MAIL,
+    }),
+  ],
   providers: [
     PasswordService,
     {
       provide: HashingProvider,
       useClass: BcryptProvider,
     },
+    PasswordResetMailProcessor,
   ],
   controllers: [PasswordController],
 })
