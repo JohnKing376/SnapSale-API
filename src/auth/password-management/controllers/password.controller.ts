5f98797101caf07a1d7dd9c7c1d7c7f7d5f4f5a6
JohnKing376 <johnolufemikingiyanu@gmail.com>
2025-05-14 23:20:42 +0100

    feat: create password.controller

--- src/auth/password-management/controllers/password.controller.ts
+++ src/auth/password-management/controllers/password.controller.ts
@@ -4,6 +4,7 @@ import {
   Controller,
   HttpStatus,
   Patch,
+  Post,
   UseInterceptors,
 } from '@nestjs/common';
 import { GetUser } from '../../decorators/get-user.decorator';
@@ -12,6 +13,11 @@ import { ChangePasswordDto } from '../dtos/change-password.dto';
 import { PasswordService } from '../providers/password.service';
 import { ResponseMeta } from '../../../common/decorators/response-meta.decorator';
 import { SystemMessages } from '../../../common/messages/system.messages';
+import { GenerateResetTokenDto } from '../dtos/generate-reset-token.dto';
+import { AuthType } from '../../enums/auth-type.enums';
+import { Auth } from '../../decorators/auth.decorator';
+import { VerifyResetTokenDto } from '../dtos/verify-reset-token.dto';
+import { ResetPasswordDto } from '../dtos/reset-password.dto';
 
 @Controller('password-management')
 export class PasswordController {
@@ -35,6 +41,43 @@ export class PasswordController {
     return await this.passwordService.changePassword(
       authUser.sub,
       changePasswordDto,
+    );
+  }
+
+  @Auth(AuthType.NONE)
+  @ResponseMeta({
+    message: SystemMessages.SUCCESS.PASSWORD_RESET_EMAIL_SENT,
+    statusCode: HttpStatus.OK,
+  })
+  @Post('send-password-reset-token')
+  public async sendResetToken(
+    @Body() generateResetTokenDto: GenerateResetTokenDto,
+  ) {
+    return await this.passwordService.sendResetCode(generateResetTokenDto);
+  }
+
+  @Auth(AuthType.NONE)
+  @ResponseMeta({
+    message: SystemMessages.SUCCESS.OTP_VERIFIED,
+    statusCode: HttpStatus.OK,
+  })
+  @Post('verify-reset-token')
+  public async verifyToken(@Body() verifyTokenDto: VerifyResetTokenDto) {
+    return await this.passwordService.verifyResetToken(verifyTokenDto);
+  }
+
+  @Patch('reset-password')
+  @ResponseMeta({
+    message: SystemMessages.SUCCESS.PASSWORD_RESET_SUCCESSFUL,
+    statusCode: HttpStatus.OK,
+  })
+  public async resetPassword(
+    @GetUser() activeUser: GetUserData,
+    @Body() resetPasswordDto: ResetPasswordDto,
+  ) {
+    return await this.passwordService.resetPassword(
+      activeUser,
+      resetPasswordDto,
     );
   }
 }
