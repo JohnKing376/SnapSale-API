96b1f64d97ecddae1e7735d434f8dfa311f360e4
JohnKing376 <johnolufemikingiyanu@gmail.com>
2025-06-03 21:41:27 +0100

    feat: add create-customer-response.interface for type safety

---
+++ src/infrastructure/payment-providers/paystack/interfaces/create-customer-response.interface.ts
@@ -0,0 +1,14 @@
+export interface CreateCustomerResponse {
+  status: boolean;
+  message: string;
+  data: {
+    email: string;
+    integration: number;
+    domain: string;
+    customer_code: string;
+    id: number;
+    identified: boolean;
+    identifications: any;
+    createdAt: string;
+  };
+}
