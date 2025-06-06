42885689ba33b28cb36ff9b0324bd3e83ed16e16
JohnKing376 <johnolufemikingiyanu@gmail.com>
2025-06-06 16:28:21 +0100

    feat: initialize paystack services

--- src/infrastructure/payment-providers/paystack/paystack.service.ts
+++ src/infrastructure/payment-providers/paystack/paystack.service.ts
@@ -1,8 +1,10 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { firstValueFrom, Observable } from 'rxjs';
-import { AxiosResponse } from 'axios';
+import { firstValueFrom } from 'rxjs';
 import { HttpService } from '@nestjs/axios';
 import { CreateCustomerResponse } from './interfaces/create-customer-response.interface';
+import { InitializeTransactionResponse } from './interfaces/initialize-transaction-response.interface';
+import { VerifyTransactionResponse } from './interfaces/verify-transaction-response.interface';
+import { ChargeAuthorizationResponse } from './interfaces/charge-authorization-response.interface';
 
 @Injectable()
 export class PaystackService {
@@ -27,11 +29,16 @@ export class PaystackService {
       }),
     );
 
-    if (httpResponse.status !== 200) {
-      console.log(
-        new HttpException('Paystack Provider Error', HttpStatus.BAD_GATEWAY, {
-          cause: 'Error interacting with paystack provider',
-        }),
+    if ((httpResponse.status as HttpStatus) !== HttpStatus.OK) {
+      console.error(
+        'Paystack Provider Error',
+        new HttpException(
+          `${JSON.stringify(httpResponse.data, null, 2)}`,
+          HttpStatus.BAD_GATEWAY,
+          {
+            cause: httpResponse.data,
+          },
+        ),
       );
       return null;
     }
@@ -42,6 +49,299 @@ export class PaystackService {
       infrastructureResults: responseData.data,
       customerInformation: {
         customerCode: responseData.data.customer_code,
+      },
+    };
+  }
+
+  public async initializeTransaction(options: {
+    email: string;
+    amount: number;
+    reference?: string;
+    callback_url?: string;
+  }): Promise<{
+    infrastructureResults: Record<string, any>;
+    transactionInformation: {
+      authorizationUrl: string;
+      access_code: string;
+      reference: string;
+    };
+  } | null> {
+    const payload = {
+      ...options,
+      amount: options.amount * 100, // convert to kobo
+    };
+
+    const httpResponse = await firstValueFrom(
+      this.httpService.post(`${this.baseUrl}/transaction/initialize`, payload, {
+        headers: {
+          Authorization: `Bearer ${process.env.PAYSTACK_SECRET}`,
+          'Content-Type': 'application/json',
+        },
+      }),
+    );
+
+    if (httpResponse.status !== 200) {
+      console.error(
+        'Paystack Provider Error',
+        new HttpException(
+          `${JSON.stringify(httpResponse.data, null, 2)}`,
+          HttpStatus.BAD_GATEWAY,
+          {
+            cause: httpResponse.data,
+          },
+        ),
+      );
+      return null;
+    }
+
+    const responseData = httpResponse.data as InitializeTransactionResponse;
+
+    return {
+      infrastructureResults: responseData.data,
+      transactionInformation: {
+        authorizationUrl: responseData.data.authorization_url,
+        access_code: responseData.data.access_code,
+        reference: responseData.data.reference,
+      },
+    };
+  }
+
+  public async verifyTransaction(options: { reference: string }): Promise<
+    | {
+        transactionStatus: 'success';
+        infrastructureResults: Record<string, any>;
+
+        transactionInformation: {
+          referenceCode: string;
+
+          transactionAmount: number;
+
+          transactionDate: string;
+        };
+        customerCode: string;
+      }
+    | {
+        transactionStatus: 'pending' | 'failed';
+        infrastructureResults: Record<string, any>;
+
+        transactionInformation: {
+          referenceCode: string;
+
+          transactionAmount: number;
+
+          transactionDate: string;
+        };
+        customerCode: string;
+      }
+    | null
+  > {
+    const httpResponse = await firstValueFrom(
+      this.httpService.get(`${this.baseUrl}/verify/${options.reference}`, {
+        headers: {
+          Authorization: `Bearer ${process.env.PAYSTACK_SECRET}`,
+          'Content-Type': 'application/json',
+        },
+      }),
+    );
+
+    if ((httpResponse.status as HttpStatus) !== HttpStatus.OK) {
+      console.error(
+        'Paystack Provider Error',
+        new HttpException(
+          `${JSON.stringify(httpResponse.data, null, 2)}`,
+          HttpStatus.BAD_GATEWAY,
+          {
+            cause: httpResponse.data,
+          },
+        ),
+      );
+      return null;
+    }
+
+    const responseData = httpResponse as VerifyTransactionResponse;
+
+    const transactionStatus = responseData.data.status;
+
+    if (transactionStatus === 'abandoned' || transactionStatus === 'pending') {
+      console.error(
+        'Paystack Provider Error',
+        new HttpException(
+          `${JSON.stringify(httpResponse.data, null, 2)}`,
+          HttpStatus.BAD_GATEWAY,
+          {
+            cause: httpResponse.data,
+          },
+        ),
+      );
+
+      return {
+        transactionStatus: 'pending',
+        infrastructureResults: responseData.data,
+        transactionInformation: {
+          referenceCode: responseData.data.referenceCode,
+          transactionAmount: responseData.data.amount / 100,
+          transactionDate: responseData.data.paid_at.toISOString(),
+        },
+        customerCode: responseData.data.customer.customer_code,
+      };
+    }
+
+    if (transactionStatus === 'failed') {
+      console.error(
+        'Paystack Provider Error',
+        new HttpException(
+          `${JSON.stringify(httpResponse.data, null, 2)}`,
+          HttpStatus.BAD_GATEWAY,
+          {
+            cause: httpResponse.data,
+          },
+        ),
+      );
+
+      return {
+        transactionStatus: 'failed',
+        infrastructureResults: responseData.data,
+        transactionInformation: {
+          referenceCode: responseData.data.referenceCode,
+          transactionAmount: responseData.data.amount / 100,
+          transactionDate: responseData.data.paid_at.toISOString(),
+        },
+        customerCode: responseData.data.customer.customer_code,
+      };
+    }
+
+    return {
+      transactionStatus: 'success',
+      infrastructureResults: responseData.data,
+      transactionInformation: {
+        referenceCode: responseData.data.referenceCode,
+        transactionAmount: responseData.data.amount / 100,
+        transactionDate: responseData.data.paid_at.toISOString(),
+      },
+      customerCode: responseData.data.customer.customer_code,
+    };
+  }
+
+  public async chargeAuthorization(data: {
+    amount: number;
+    email: string;
+    authorization_code: string;
+  }): Promise<
+    | {
+        transactionStatus: 'success';
+        infrastructureResults: Record<string, any>;
+
+        transactionInformation: {
+          referenceCode: string;
+
+          transactionAmount: number;
+
+          transactionDate: string;
+        };
+      }
+    | {
+        transactionStatus: 'pending' | 'failed';
+        infrastructureResults: Record<string, any>;
+
+        transactionInformation: {
+          referenceCode: string;
+
+          transactionAmount: number;
+
+          transactionDate: string;
+        };
+      }
+    | null
+  > {
+    const payload = {
+      ...data,
+      amount: data.amount * 100,
+    };
+
+    const httpResponse = await firstValueFrom(
+      this.httpService.post(
+        `${this.baseUrl}/transaction/charge_authorization`,
+        payload,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.PAYSTACK_SECRET}`,
+            'Content-Type': 'application/json',
+          },
+        },
+      ),
+    );
+
+    if ((httpResponse.status as HttpStatus) !== HttpStatus.OK) {
+      console.error(
+        'Paystack Provider Error',
+        new HttpException(
+          `${JSON.stringify(httpResponse.data, null, 2)}`,
+          HttpStatus.BAD_GATEWAY,
+          {
+            cause: httpResponse.data,
+          },
+        ),
+      );
+      return null;
+    }
+
+    const responseData = httpResponse.data as ChargeAuthorizationResponse;
+
+    const transactionStatus = responseData.data.status;
+
+    if (transactionStatus === 'abandoned' || transactionStatus === 'pending') {
+      console.error(
+        'Paystack Provider Error',
+        new HttpException(
+          `${JSON.stringify(httpResponse.data, null, 2)}`,
+          HttpStatus.BAD_GATEWAY,
+          {
+            cause: httpResponse.data,
+          },
+        ),
+      );
+
+      return {
+        transactionStatus: 'pending',
+        infrastructureResults: responseData.data,
+        transactionInformation: {
+          referenceCode: responseData.data.referenceCode,
+          transactionAmount: responseData.data.amount / 100,
+          transactionDate: responseData.data.paid_at.toISOString(),
+        },
+      };
+    }
+
+    if (transactionStatus === 'failed') {
+      console.error(
+        'Paystack Provider Error',
+        new HttpException(
+          `${JSON.stringify(httpResponse.data, null, 2)}`,
+          HttpStatus.BAD_GATEWAY,
+          {
+            cause: httpResponse.data,
+          },
+        ),
+      );
+
+      return {
+        transactionStatus: 'failed',
+        infrastructureResults: responseData.data,
+        transactionInformation: {
+          referenceCode: responseData.data.referenceCode,
+          transactionAmount: responseData.data.amount / 100,
+          transactionDate: responseData.data.paid_at.toISOString(),
+        },
+      };
+    }
+
+    return {
+      transactionStatus: 'success',
+      infrastructureResults: responseData.data,
+      transactionInformation: {
+        referenceCode: responseData.data.referenceCode,
+        transactionAmount: responseData.data.amount / 100,
+        transactionDate: responseData.data.paid_at.toISOString(),
       },
     };
   }
