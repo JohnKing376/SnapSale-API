64c4b1462cc621ef80d99f860cc4567df65221f2
John O.King <110711668+JohnKing376@users.noreply.github.com>
2025-06-11 19:50:34 +0100

    chore: update delivery-module

--- src/delivery/delivery.module.ts
+++ src/delivery/delivery.module.ts
@@ -3,10 +3,14 @@ import { DeliveryService } from './providers/delivery.service';
 import { DeliveryController } from './controllers/delivery.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Delivery } from './entities/delivery.entity';
+import { UsersModule } from '../users/users.module';
+import { OrderModule } from '../order/order.module';
+import { CreateDeliverUseCase } from './use-cases/create-delivery/create-delivery-use-case';
+import UpdateDeliveryUseCase from './use-cases/update-delivery/update-delivery-use-case';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Delivery])],
-  providers: [DeliveryService],
+  imports: [TypeOrmModule.forFeature([Delivery]), UsersModule, OrderModule],
+  providers: [DeliveryService, CreateDeliverUseCase, UpdateDeliveryUseCase],
   controllers: [DeliveryController],
 })
 export class DeliveryModule {}
